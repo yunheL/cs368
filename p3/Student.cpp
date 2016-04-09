b57e888aa7f925f4cdac7391f936cc3b83279dfe
yunheL <liu348@wisc.edu>
2016-04-09 14:59:16 -0500

    student.cpp finished

--- p3/Student.cpp
+++ p3/Student.cpp
@@ -20,3 +20,75 @@ Student::Student(int ID, int cr, double grPtAv) :
 studentID(ID), credits(cr), GPA(grPtAv){
   cout<< "default 3-arg constructor" << endl;
 }
+
+//accessor - ID
+int Student::getID() const
+{
+  return studentID;
+}
+
+//accessor - credits
+int Student::getCredits() const
+{
+  return credits;
+}
+
+//accessor - GPA
+double Student::getGPA() const
+{
+  return GPA;
+}
+
+//update()
+void Student::update(char grade, int cr)
+{
+  float numericalGrade = 0.0;
+  float add_points = 0.0;
+  float old_points = 0.0;
+  float total_points = 0.0;
+  int total_cr = 0;
+  float updatedGPA = 0.0;
+
+  //convert letter grade to numerical grade
+  if(grade == 'A' || grade == 'a')
+    numericalGrade = 4.0;
+  else if(grade == 'B' || grade == 'b')
+    numericalGrade = 3.0;
+  else if(grade == 'C' || grade == 'c')
+    numericalGrade = 2.0;
+  else if(grade == 'D' || grade == 'd')
+    numericalGrade = 1.0;
+  else if(grade == 'F' || grade == 'f')
+    numericalGrade = 0.0;
+
+  //calculate the additional points
+  add_points = numericalGrade * cr;
+  //cout << "add_points is " << add_points << endl;
+
+  //calculate the old points
+  old_points = GPA * credits;
+  //cout << "old_points is " << old_points << endl;
+
+  //calculate the total points
+  total_points = add_points + old_points;
+  //cout << "total_point is " << total_points << endl;
+
+  //calculate the total credits
+  total_cr = credits + cr;
+  //cout << "total_cr is " << total_cr << endl;
+
+  //calculate the updated GPA
+  updatedGPA = ((float)(total_points))/((float)(total_cr));
+  //cout << "updatedGPA is " << updatedGPA << endl;
+
+  //upate GPA
+  GPA = updatedGPA;
+
+  //update Credit
+  credits = credits + cr;
+}
+
+void Student::print() const
+{
+  cout << studentID << ',' << credits << ',' << GPA;
+}
--- p3/main.cpp
+++ p3/main.cpp
@@ -14,5 +14,17 @@ int main(int argc, char* argv[])
   Student st1(1);
   Student st2(2, 8, 4.0);
 
+  cout << "st2_ID = " << st2.getID() << endl;
+  cout << "st2_credit = " << st2.getCredits() << endl;
+  cout << "st2_GPA = " << st2.getGPA() << endl;
+
+  st2.update('f', 2);
+  cout << "st2_ID = " << st2.getID() << endl;
+  cout << "st2_credit = " << st2.getCredits() << endl;
+  cout << "st2_GPA = " << st2.getGPA() << endl;
+
+  st1.print();
+  st2.print();
+
   return 0;
 }
