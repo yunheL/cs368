75075b167fd4b68137212f37f643206297eab8e8
yunheL <liu348@wisc.edu>
2016-04-10 00:55:34 -0500

    step1 finished

--- p3/SortedList.cpp
+++ p3/SortedList.cpp
@@ -1,1 +1,114 @@
+#include <iostream>
+#include "SortedList.h"
+
 using namespace std;
+
+//no-arg constructor
+SortedList::SortedList() :
+head(new Listnode)
+{
+  cout << "list no-arg constructor" << endl;
+}
+
+//insert - done
+bool SortedList::insert(Student *s)
+{
+  Listnode* s_node;
+  s_node = new Listnode;
+  s_node->student = s;  
+
+  Listnode* curr = head;
+  Listnode* prev = head;
+  bool first_loop = true;
+
+  while(curr->next != NULL)
+  {
+    curr = curr->next;
+    if(!first_loop)
+    {
+      prev = prev->next;
+    }
+    first_loop = false;
+
+    if(curr->student->getID() == s->getID())
+    {
+      return false;
+    }
+    else if(curr->student->getID() > s->getID())
+    {
+      //insert the node befre curr and after prev
+      s_node->next = curr;
+      prev->next = s_node;
+      return true;
+    }
+  }
+
+  //if the inserted node has the largest ID
+  curr->next = s_node;
+  
+  return true;
+}
+
+//find - done
+Student* SortedList::find(int studentID)
+{
+  Listnode* curr = head; 
+
+  while(curr->next != NULL)
+  {
+    curr = curr->next;
+    if(curr->student->getID() == studentID)
+      return curr->student;
+  }
+ 
+  return NULL;
+}
+
+
+//remove
+Student* SortedList::remove(int studentID)
+{
+  Listnode* curr = head;
+  Listnode* prev = head;
+//  Listnode* ret;
+  bool first_loop = true;
+
+  while(curr->next != NULL)
+  {
+    curr = curr->next;
+    if(!first_loop)
+    {
+      prev = prev->next;
+    }
+    first_loop = false;
+
+    if(curr->student->getID() == studentID)
+    {
+      //remove node
+      prev->next = curr->next;
+      //ret = curr;
+      //delete curr;
+      //return ret;
+      return curr->student;
+    }
+    else if(curr->student->getID() > studentID)
+    {
+      //already not possible to find a match
+      return NULL;
+    }
+  }
+  return NULL;
+}
+
+//print - done
+void SortedList::print() const
+{
+  Listnode* curr = head;
+
+  while(curr->next != NULL)
+  {
+    curr = curr->next;
+    (*curr->student).print();
+    cout << endl;
+  }
+}
--- p3/main.cpp
+++ p3/main.cpp
@@ -13,6 +13,8 @@ int main(int argc, char* argv[])
   Student st0;
   Student st1(1);
   Student st2(2, 8, 4.0);
+  Student st3(3, 3, 4.0);
+  Student st4(4, 4, 4.0);
 
   cout << "st2_ID = " << st2.getID() << endl;
   cout << "st2_credit = " << st2.getCredits() << endl;
@@ -25,6 +27,29 @@ int main(int argc, char* argv[])
 
   st1.print();
   st2.print();
+  cout << endl << "=================================" << endl;
+
+  SortedList list0;
+
+  Student* st2_ptr = &st2;
+  Student* st3_ptr = &st3;
+  Student* st4_ptr = &st4;
+
+  cout << "insert id=2: " << list0.insert(st2_ptr) << endl;
+  cout << "insert id=2: " << list0.insert(st2_ptr) << endl;
+  cout << "insert id=4: " << list0.insert(st4_ptr) << endl;
+
+  cout << "insert id=4: " << list0.insert(st4_ptr) << endl;
+  cout << "insert id=3: " << list0.insert(st3_ptr) << endl;
+  cout << "insert id=3: " << list0.insert(st3_ptr) << endl;
+
+  cout << "remove id=4: " << list0.remove(4) << "; expected: " << &st4 << endl;
+  cout << "remove id=2: " << list0.remove(2) << "; expected: " << &st2 << endl;
+  cout << "remove id=2: " << list0.remove(2) << "; expected: " << NULL << endl;
+
+  list0.print();
+  cout << "find2: " << list0.find(2) << "; expected: " << NULL << endl;
+  cout << "find3: " << list0.find(3) << "; expected: " << &st3 << endl;
 
   return 0;
 }
